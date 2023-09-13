606e64f692c469f5236efb7c981f4c4dc244eda9
Michal Kowalewski <michalkowalewski88@gmail.com>
2023-09-13 17:51:16 +0200

    create top nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
 import {Suspense} from 'react';
 import {useTranslation} from 'react-i18next'
-import './App.css'
+import './App.scss'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
 
 const locales = {
-    en: {title: 'English'},
-    pl: {title: 'Polski'}
+    en: {title: 'EN'},
+    pl: {title: 'PL'}
 };
 
 function App() {
     const {t, i18n} = useTranslation();
     return (
-        <div>
-            <ul>
-                {Object.keys(locales).map(locale => (
-                    <li key={locale}>
-                        <button style={{fontWeight: i18n.resolvedLanguage === locale ? 'bold' : 'normal'}} type="submit"
-                                onClick={() => i18n.changeLanguage(locale)}>
-                            {locales[locale].title}
-                        </button>
-                    </li>
-                ))}
-            </ul>
-            <h1>{t('header')}</h1>
-        </div>
+        <>
+            <Navbar collapseOnSelect expand='md' fixed='top'>
+                <Container className="d-flex justify-content-between">
+                    <Navbar.Brand  href="#"><img className="logo" src="../public/logo.jpg" alt="Working Capital"/></Navbar.Brand>
+                    <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+                    <Navbar.Collapse className="justify-content-end" id='responsive-navbar-nav'>
+                            <Nav>
+                                <Nav.Link className="navlink" href='/#'>HOME</Nav.Link>
+                                <Nav.Link href='#about'>About Us</Nav.Link>
+                                <Nav.Link href='#team'>Team</Nav.Link>
+                                <Nav.Link href='#strategy'>Strategy</Nav.Link>
+                                <Nav.Link href='#portfolio'>Portfolio</Nav.Link>
+                                <Nav.Link href='#contact'>Contact Us</Nav.Link>
+                            </Nav>
+
+                    </Navbar.Collapse>
+                    <ul className="ms-4" >
+                        {Object.keys(locales).map(locale => (
+                            <li key={locale}>
+                                <button className="language-button" style={{width:'50px', fontWeight: i18n.resolvedLanguage === locale ? 'bold' : 'normal'}} type='submit'
+                                        onClick={() => i18n.changeLanguage(locale)}>
+                                    {locales[locale].title}
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </Container>
+            </Navbar>
+
+                <div>
+
+                    <h1>{t('header')}</h1>
+                </div>
+
+
+        </>
+
 
     )
 }
 
 export default function WrappedApp() {
     return (
-        <Suspense fallback="...">
+        <Suspense fallback='...'>
             <App/>
         </Suspense>
     )
