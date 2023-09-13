ed99deedd0fc9b1fcece21b54cdfffb18ac8d431
Michal Kowalewski <michalkowalewski88@gmail.com>
2023-09-13 13:09:18 +0200

    create translation logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-
+import {Suspense} from 'react';
+import {useTranslation} from 'react-i18next'
 import './App.css'
 
+const locales = {
+    en: {title: 'English'},
+    pl: {title: 'Polski'}
+};
+
 function App() {
+    const {t, i18n} = useTranslation();
+    return (
+        <div>
+            <ul>
+                {Object.keys(locales).map(locale => (
+                    <li key={locale}>
+                        <button style={{fontWeight: i18n.resolvedLanguage === locale ? 'bold' : 'normal'}} type="submit"
+                                onClick={() => i18n.changeLanguage(locale)}>
+                            {locales[locale].title}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+            <h1>{t('header')}</h1>
+        </div>
 
-  return (
-      <div>
-       app
-      </div>
-
-  )
+    )
 }
 
-export default App
+export default function WrappedApp() {
+    return (
+        <Suspense fallback="...">
+            <App/>
+        </Suspense>
+    )
+}
