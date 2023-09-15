1ffec3fa793cb55f9f9111faab203c91b89f486e
Michal Kowalewski <michalkowalewski88@gmail.com>
2023-09-15 13:36:26 +0200

    top section change

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import { Suspense } from "react";
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container,Row, Col } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import Navigation from "./components/Navigation/Navigation.jsx";
 import Welcome from "./components/Welcome/Welcome.jsx";
 
-
-
 function App() {
   return (
-      <div className="App">
-          <Navigation/>
-          <Welcome/>
-      </div>
-
-
+    <div className="App">
+      <Navigation />
+      <Welcome />
+    </div>
   );
 }
 
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
 import "./Welcome.scss";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 const locales = {
   en: { title: "EN" },
@@ -10,25 +10,14 @@ const locales = {
 function Welcome() {
   const { t } = useTranslation();
   return (
-          <Container>
-              <div className="welcome-section__row-wrapper">
-                  <Row className="welcome-section__row">
-                      <Col className="welcome-section__column" sm={6}>
-                          <div className="welcome-section__text-container">
-                              <h2 className="welcome-section__header"> {t("welcome")}</h2>
-                              <h3 className="welcome-section__text">{t("seeding")}</h3>
-                          </div>
-                      </Col>
-                      <Col className="welcome-section__column"  sm={6}>
-                              <img className="welcome-section__image" src="images/people.webp" alt="people"/>
-
-                      </Col>
-                  </Row>
-              </div>
-
-          </Container>
-
-
+    <Container className="welcome-section">
+      <div className="welcome-section__row-wrapper">
+        <div className="welcome-section__text-container">
+          <h2 className="welcome-section__header"> {t("welcome")}</h2>
+          <h3 className="welcome-section__text">{t("seeding")}</h3>
+        </div>
+      </div>
+    </Container>
   );
 }
 
