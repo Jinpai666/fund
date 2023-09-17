b9a247b4868173d3b8e45120c1cd718a4508c347
Michal Kowalewski <michalkowalewski88@gmail.com>
2023-09-17 11:40:13 +0200

    people section reworked

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -14,48 +14,97 @@ function Team({ t }) {
                      </h2>
 
                      <Row className="team__columns">
-                         <Col xs={12} md={6} lg={3} xl={2}>
+                         <Col xs={12} md={6} lg={4}>
                              <div className="team__column">
                                  <img className="team__portrait" src="images/person1.jpg" alt="person"/>
-                                <p className="team__text">{t("person1")}</p>
+                                 <div className="team__text">
+                                     <p className="team__paragraph">{t("person1")}</p>
+                                     <p className="team__paragraph">{t("person1text1")}</p>
+                                     <p className="team__paragraph">{t("person1text2")}</p>
+                                     <p className="team__paragraph">{t("person1text3")}</p>
+                                     <p className="team__paragraph">{t("person1text4")}</p>
+                                     <p className="team__paragraph">{t("person1text5")}</p>
+                                 </div>
+
                              </div>
 
                          </Col>
-                         <Col xs={12} md={6} lg={3} xl={2}>
+                         <Col xs={12} md={6} lg={4}>
                              <div className="team__column">
                                  <img className="team__portrait" src="images/person1.jpg" alt="person"/>
-                                <p className="team__text">{t("person1")}</p>
+                                 <div className="team__text">
+                                     <p className="team__paragraph">{t("person1")}</p>
+                                     <p className="team__paragraph">{t("person1text1")}</p>
+                                     <p className="team__paragraph">{t("person1text2")}</p>
+                                     <p className="team__paragraph">{t("person1text3")}</p>
+                                     <p className="team__paragraph">{t("person1text4")}</p>
+                                     <p className="team__paragraph">{t("person1text5")}</p>
+                                 </div>
+
                              </div>
 
                          </Col>
-                         <Col xs={12} md={6} lg={3} xl={2}>
+                         <Col xs={12} md={6} lg={4}>
                              <div className="team__column">
                                  <img className="team__portrait" src="images/person1.jpg" alt="person"/>
-                                <p className="team__text">{t("person1")}</p>
+                                 <div className="team__text">
+                                     <p className="team__paragraph">{t("person1")}</p>
+                                     <p className="team__paragraph">{t("person1text1")}</p>
+                                     <p className="team__paragraph">{t("person1text2")}</p>
+                                     <p className="team__paragraph">{t("person1text3")}</p>
+                                     <p className="team__paragraph">{t("person1text4")}</p>
+                                     <p className="team__paragraph">{t("person1text5")}</p>
+                                 </div>
+
                              </div>
 
                          </Col>
-                         <Col xs={12} md={6} lg={3} xl={2}>
+                         <Col xs={12} md={6} lg={4}>
                              <div className="team__column">
                                  <img className="team__portrait" src="images/person1.jpg" alt="person"/>
-                                <p className="team__text">{t("person1")}</p>
+                                 <div className="team__text">
+                                     <p className="team__paragraph">{t("person1")}</p>
+                                     <p className="team__paragraph">{t("person1text1")}</p>
+                                     <p className="team__paragraph">{t("person1text2")}</p>
+                                     <p className="team__paragraph">{t("person1text3")}</p>
+                                     <p className="team__paragraph">{t("person1text4")}</p>
+                                     <p className="team__paragraph">{t("person1text5")}</p>
+                                 </div>
+
                              </div>
 
                          </Col>
-                         <Col xs={12} md={6} lg={3} xl={2}>
+                         <Col xs={12} md={6} lg={4}>
                              <div className="team__column">
                                  <img className="team__portrait" src="images/person1.jpg" alt="person"/>
-                                <p className="team__text">{t("person1")}</p>
+                                 <div className="team__text">
+                                     <p className="team__paragraph">{t("person1")}</p>
+                                     <p className="team__paragraph">{t("person1text1")}</p>
+                                     <p className="team__paragraph">{t("person1text2")}</p>
+                                     <p className="team__paragraph">{t("person1text3")}</p>
+                                     <p className="team__paragraph">{t("person1text4")}</p>
+                                     <p className="team__paragraph">{t("person1text5")}</p>
+                                 </div>
+
                              </div>
 
                          </Col>
-                         <Col xs={12} md={6} lg={3} xl={2}>
+                         <Col xs={12} md={6} lg={4}>
                              <div className="team__column">
                                  <img className="team__portrait" src="images/person1.jpg" alt="person"/>
-                                <p className="team__text">{t("person1")}</p>
+                                 <div className="team__text">
+                                     <p className="team__paragraph">{t("person1")}</p>
+                                     <p className="team__paragraph">{t("person1text1")}</p>
+                                     <p className="team__paragraph">{t("person1text2")}</p>
+                                     <p className="team__paragraph">{t("person1text3")}</p>
+                                     <p className="team__paragraph">{t("person1text4")}</p>
+                                     <p className="team__paragraph">{t("person1text5")}</p>
+                                 </div>
+
                              </div>
 
                          </Col>
+
 
                      </Row>
 
