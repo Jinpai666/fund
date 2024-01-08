dde2164edba87d0009915ca2af31572e9dfd21ca
Michał Kowalewski <michalkowalewski88@gmail.com>
2024-01-08 14:10:05 +0100

    fix

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -10,100 +10,102 @@ function Navigation({ t, i18n }) {
   };
 
   return (
-    <nav>
-      <Container className="navigation">
-        <div className="navigation__left-side">
-          <img
-            className="navigation__logo"
-            src="images/logo2.svg"
-            alt="Working Capital"
-          />
-          <ul className="navigation__links">
-            <li>
-              <Link
-                className="navigation__link navigation__link_home"
-                to="home"
-                smooth={true}
-                duration={500}
-              >
-                {t("home")}
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="navigation__link"
-                to="about"
-                offset={-100}
-                smooth={true}
-                duration={500}
-              >
-                {t("about")}
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="navigation__link"
-                to="portfolio"
-                offset={-100}
-                smooth={true}
-                duration={500}
-              >
-                {t("portfolio")}
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="navigation__link"
-                to="team"
-                offset={-100}
-                smooth={true}
-                duration={500}
-              >
-                {t("team")}
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="navigation__link"
-                to="strategy"
-                offset={-100}
-                smooth={true}
-                duration={500}
-              >
-                {t("strategy")}
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="navigation__link"
-                to="contact"
-                offset={-100}
-                smooth={true}
-                duration={500}
-              >
-                {t("contact")}
-              </Link>
-            </li>
-          </ul>
-        </div>
+    <nav className="backhround-white">
+      <div className="navigation">
+        <div className="navigation-wrapper">
+          <div className="navigation__left-side">
+            <img
+              className="navigation__logo"
+              src="images/logo2.svg"
+              alt="Working Capital"
+            />
+            <ul className="navigation__links">
+              <li>
+                <Link
+                  className="navigation__link navigation__link_home"
+                  to="home"
+                  smooth={true}
+                  duration={500}
+                >
+                  {t("home")}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="navigation__link"
+                  to="about"
+                  offset={-100}
+                  smooth={true}
+                  duration={500}
+                >
+                  {t("about")}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="navigation__link"
+                  to="portfolio"
+                  offset={-100}
+                  smooth={true}
+                  duration={500}
+                >
+                  {t("portfolio")}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="navigation__link"
+                  to="team"
+                  offset={-100}
+                  smooth={true}
+                  duration={500}
+                >
+                  {t("team")}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="navigation__link"
+                  to="strategy"
+                  offset={-100}
+                  smooth={true}
+                  duration={500}
+                >
+                  {t("strategy")}
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="navigation__link"
+                  to="contact"
+                  offset={-100}
+                  smooth={true}
+                  duration={500}
+                >
+                  {t("contact")}
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-        <div className="navigation__right-side">
-          <BurgerMenu t={t} />
+          <div className="navigation__right-side">
+            <BurgerMenu t={t} />
 
-          <ul className="ms-4 navigation__buttons">
-            <div className="navigation__button-wrapper">
-              <div
-                onClick={() =>
-                  langChange(i18n.resolvedLanguage === "pl" ? "en" : "pl")
-                }
-                className="navigation__language-changer"
-              >
-                {i18n.resolvedLanguage === "pl" ? "en" : "pl"}
+            <ul className="ms-4 navigation__buttons">
+              <div className="navigation__button-wrapper">
+                <div
+                  onClick={() =>
+                    langChange(i18n.resolvedLanguage === "pl" ? "en" : "pl")
+                  }
+                  className="navigation__language-changer"
+                >
+                  {i18n.resolvedLanguage === "pl" ? "en" : "pl"}
+                </div>
               </div>
-            </div>
-          </ul>
+            </ul>
+          </div>
         </div>
-      </Container>
+      </div>
     </nav>
   );
 }
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -4,19 +4,19 @@ import { Container } from "react-bootstrap";
 function Welcome({ t }) {
   return (
     <section id="home">
-      <Container>
-        <div className="welcome-section">
+      <div className="welcome-section">
+        <Container>
           <div className="welcome-section__text-container">
             <div className="welcome-section__text-wrapper">
               <h1
                 className="welcome-section__header"
                 dangerouslySetInnerHTML={{ __html: t("welcome") }}
-              ></h1>
+              />
               <p className="welcome-section__text">{t("seeding")}</p>
             </div>
           </div>
-        </div>
-      </Container>
+        </Container>
+      </div>
     </section>
   );
 }
