2acd6323b237495622d7826108757fc2e7a5019e
Michał Kowalewski <michalkowalewski88@gmail.com>
2024-01-16 17:19:30 +0100

    partners change

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -1,11 +1,14 @@
 import "./Partners.scss";
+import SectionHeader from "../SectionHeader/SectionHeader";
+
 import { Row, Col, Container } from "react-bootstrap";
 function Partners({ t }) {
   return (
     <section id="partners">
       <Container>
         <div className="partners">
-          <h2 className="partners__header">{t("partnersHeader")}</h2>
+          <SectionHeader header={t("partnersHeader")} />
+
           <Row className="partners__row">
             <Col className="partners__logo-wrapper" xs={6} md={4} lg={2}>
               <div className="partners__logo" />
